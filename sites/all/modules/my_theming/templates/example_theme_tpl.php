ed4d8ed2bfd41dab63000c323bd2037754bcdffe
webik <webik911@gmail.com>
2013-03-19 18:02:16 +0200

    module my_theming

---
+++ sites/all/modules/my_theming/templates/example_theme_tpl.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * Customize confirmation screen after successful submission.
+ */
+?>
+<a href="" class="my-example-template"><?php print $name; ?></a>
+    <p><?php print t('Thank you, your submission has been received.'); ?></p>
